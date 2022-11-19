c075493cad028f92787c37570fbf84bee894def6
Maksim Omelchenko <omelchenk0.m89@yandex.ru>
2022-11-20 02:19:53 +0700

    Четвертая проектная работа(5)

--- scripts/script.js
+++ scripts/script.js
@@ -3,20 +3,20 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
 const popupOpenButtonElement = document.querySelector('.profile__button-edit');
 const formElement = popupElement.querySelector('.popup__form');
-const nameInput = popupElement.querySelector('.popup__name');
-const jobInput = popupElement.querySelector('.popup__profession');
+const nameInput = document.getElementById('name');
+const jobInput = document.getElementById('profession');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__profession');
 
 // Открытие попапа
 const openPopup = function() {
-  popupElement.classList.add('popup__open');
+  popupElement.classList.add('popup_is-opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 // закрытие попапа
 const closePopup = function() {
-    popupElement.classList.remove('popup__open');
+    popupElement.classList.remove('popup_is-opened');
   }
 
 // закрытие попапа по клику на оверлей
